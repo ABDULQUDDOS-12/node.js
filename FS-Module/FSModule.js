d7b600772cba81c10879eb4850a5360939832d72
Abdul quddos <abdulquddoshafiz@gmail.com>
2023-07-28 23:17:15 +0500

    http module

--- FS-Module/FSModule.js
+++ FS-Module/FSModule.js
@@ -1,6 +1,12 @@
-const {readFileSync,writeFileSync} = require('fs')
-const first = readFileSync('../content/first.txt','utf8')
-const second = readFileSync('../content/second.txt','utf8')
-console.log(first,second)
+const { readFileSync, writeFileSync } = require("fs");
+const first = readFileSync("../content/first.txt", "utf8");
+const second = readFileSync("../content/second.txt", "utf8");
+console.log(first, second);
 
 //if the file is already created then it will be ok otherwise it will create a file for you
+writeFileSync(
+  "../content/result-sync.txt",
+  `Here is the result : ${first} , ${second}`,{flag:'a'}
+);
+
+
